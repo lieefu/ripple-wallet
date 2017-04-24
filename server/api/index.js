641c2239dbb4f7df0279341cb6468d927a132b9c
Lieefu Way <lieefu@gmail.com>
2017-04-24 17:35:29 +0800

    refactor:统一处理rippleAPI的重连和异常

--- server/api/index.js
+++ server/api/index.js
@@ -3,11 +3,44 @@ const router = express.Router();
 const ripple = require("../ripple");
 /* GET api listing. */
 router.get('/', (req, res) => {
-  res.status(200).json({ok:true,message:'api works,ok'});
-});
-router.get("/serverinfo",(req,res)=>{
-    ripple.getServerInfo().then(info => {
-        res.status(200).json({ok:true,message:info});
+    res.status(200).json({
+        ok: true,
+        message: 'api works,ok'
     });
+});
+router.get("/accountinfo/:address", (req, res) => {
+    var address = req.params.address;
+    console.log(address);
+    ripple('getAccountInfo',address).then((info) => {
+        console.log("get account info", info);
+        res.status(200).json({
+            ok: true,
+            message: info
+        });
+    }).catch((error) => {
+        console.log("ripple error", error);
+        res.status(200).json({
+            ok: false,
+            message: error
+        });
+    })
+});
+router.get("/serverinfo", (req, res) => {
+    console.log("get server info begin");
+    if (!ripple.isConnected()) {
+        return res.status(200).json({
+            ok: false,
+            message: 'ripple disconnected!'
+        });
+    }
+    ripple.getServerInfo().then(
+        info => {
+            console.log("get server info");
+            res.status(200).json({
+                ok: true,
+                message: info
+            });
+        }
+    )
 })
 module.exports = router;
--- server/ripple/index.js
+++ server/ripple/index.js
@@ -9,19 +9,19 @@ api.on('error', (errorCode, errorMessage) => {
     console.log(errorCode + ': ' + errorMessage);
 });
 api.on('connected', () => {
-    console.log('connected');
+    console.log('on connected');
 });
 api.on('disconnected', (code) => {
     // code - [close code](https://developer.mozilla.org/en-US/docs/Web/API/CloseEvent) sent by the server
     // will be 1000 if this was normal closure
     console.log('disconnected, code:', code);
 });
-api.connect().then(() => {
-    console.log("ripple api connected!");
-}).then(() => {
-    //return api.disconnect();
-    console.log("ripple api connected!!");
-}).catch(console.error);
+// api.connect().then(() => {
+//     console.log("ripple api connected!");
+// }).then(() => {
+//     //return api.disconnect();
+//     console.log("ripple api connected!!");
+// }).catch(console.error);
 api.on('disconnected', (code) => {
     if (code !== 1000) {
         console.log('Connection is closed due to error.');
@@ -29,4 +29,20 @@ api.on('disconnected', (code) => {
         console.log('Connection is closed normally.');
     }
 });
-module.exports = api;
+module.exports = function(fname,...args) {
+    return new Promise(function(resolve, reject) {
+        api.connect().then(() => {
+            try{
+                if(api[fname]){
+                    api[fname](...args).then((data)=>{
+                        resolve(data);
+                    }).catch(reject);
+                }else{
+                    reject(fname+"not exist.")
+                }
+            }catch(err){
+                reject(err);
+            }
+        }).catch(reject);
+    })
+};
